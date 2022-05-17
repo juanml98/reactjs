3ca4084266bb74956dda0540c006453fd8aba5cc
juan <juanlauria98@gmail.com>
2022-05-16 23:53:30 -0300

    Clase de jsx

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        
+        <nav className='nav-enlace'>
+            <h1>CoderJS</h1>
+            
+            <div>
+                <button>Celulares</button>
+                <button>Indumentaria</button>
+                <button>Electronica</button>
+            </div>
+        </nav>
+        
+    )
+}
+
+export default NavBar
